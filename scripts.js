a70bb4f97319f3e2ba1b0c93094a644f3951c7c1
Dylan <dylanhill1228@gmail.com>
2022-03-08 17:35:57 -0600

    JS changes

--- scripts.js
+++ scripts.js
@@ -1,65 +1,67 @@
-let computerSelection;
-function computerPlay () {
-    let computersHand = Math.floor(Math.random() * 3) + 1;
-    if (computersHand === 1) {
-        return computerSelection = 'rock';
-    }
-    else if (computersHand === 2) {
-        return computerSelection = 'paper';
-    }
-    else {
-        return computerSelection = 'scissors';
-    }
-}
-console.log(computerPlay())
-
 let playerScore = 0;
 let computerScore = 0;
-function playRound (playerSelection, computerSelection) {
-    if (playerSelection === 'rock') {
-        if (computerSelection === 'rock') {
-            return 'It\'s a tie!'
-        }
-        else if (computerSelection === 'paper') {
-            return playerScore++, 'You lose! Paper beats rock.';
-        }
-        else {
-            return computerScore++, 'You win! Rock beats scissors.';
-        }
-    } 
-    if (playerSelection === 'paper') {
-        if (computerSelection === 'rock') {
-            return playerScore++, 'You win! Paper beats rock';
-        }
-        else if (computerSelection === 'paper') {
-            return 'It\'s a tie!'
-        }
-        else {
-            return computerScore, 'You lose! Scissors beats paper.';
-        }
-    } 
-    if (playerSelection === 'scissors') {
-        if (computerSelection === 'rock') {
-            return computerScore++ ,'You lose! Rock beats scissors.';
-        }
-        else if (computerSelection === 'paper') {
-            return playerScore++, 'You win! Scissors beats paper.';
-        }
-        else {
-            return 'It\'s a tie!'
-        }
-    } 
+const container = document.querySelector("#display");
+const buttons = document.querySelectorAll("#btn");
+
+function computerPlay() {
+    // Chooses a random value for computerPlay.
+    const choices = ["rock", "paper", "scissors"]
+    return choices[Math.floor(Math.random() * 3)]
+}
+
+function playRound(playerSelection, computerSelection) {
+    // Compares playerSelection and computerSelection
+    if (playerSelection === computerSelection) {
+        document.getElementById('result').innerText = ("Tie!")
+    }
+    else if (playerSelection.length - computerSelection.length === -1 || playerSelection.length - computerSelection.length === 4 || playerSelection.length - computerSelection.length === -3) {
+        computerScore++
+        document.getElementById('scoreComputer').innerText = (computerScore)
+        document.getElementById('result').innerText = (`You lost! ${computerSelection} beats ${playerSelection}.`)
+    }
+    else {
+        playerScore++
+        document.getElementById('scorePlayer').innerText = (playerScore)
+        document.getElementById('result').innerText = (`You won! ${playerSelection} beats ${computerSelection}.`)
+    }
 }
 
 function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt('Rock, Paper, or Scissors?')
-        playRound(playerSelection, computerSelection);
-        if (i === 5 && playerScore > computerScore) {
-            return 'You win!'
-        }
-        else if (i === 5 && playerScore < computerScore) {
-            return 'You lose!'
-        }
-    }
+    buttons.forEach((button1) => {
+        button1.addEventListener("click", () => {
+            let computerSelection = computerPlay();
+            if (button1.id === "btn") {
+                playRound(button1.value, computerSelection)
+            }
+            if (playerScore === 5) {
+                document.getElementById('congrats').innerText = `Congrats! You won! The results is ${playerScore} for you and ${computerScore} for the computer`
+            }
+            else if (computerScore === 5) {
+                document.getElementById('congrats').innerText = `You lost! The results is ${playerScore} for you and ${computerScore} for the computer`
+            }
+            if (playerScore === 5 || computerScore === 5) {
+                buttons.forEach((button) => {
+                    button.removeAttribute("id")
+                })
+            }
+        })
+    })
 }
+
+function playAgain() {
+    const playAgain = document.querySelector('#playAgain');
+    playAgain.addEventListener('click', () => {
+        computerScore = 0;
+        playerScore = 0;
+        document.getElementById("scorePlayer").innerText = playerScore;
+        document.getElementById("scoreComputer").innerText = computerScore;
+        document.getElementById('result').innerText = "Click any of the buttons below to start playing again."
+        document.getElementById("congrats").innerText = ""
+        buttons.forEach((button) => {
+            button.setAttribute("id", "btn")
+        })
+    });
+}
+
+game()
+playAgain()
